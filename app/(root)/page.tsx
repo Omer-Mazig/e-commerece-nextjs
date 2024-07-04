151eb7362b9ec794edd08b9594eae537a46c57cf
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-07-04 20:30:25 +0300

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p>hello admin</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}

2ad4476aacfbb09c944b84e0f0e2dd110d247338
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-07-06 11:11:39 +0300

    added api list

--- app/(dashboard)/[storeId]/(routes)/billboards/_components/billboard-client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/_components/billboard-client.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardClomun[];
@@ -33,6 +34,9 @@ export const BillboardClient = ({ data }: BillboardClientProps) => {
       <Separator />
 
       <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading title="API" description="API calls for billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
